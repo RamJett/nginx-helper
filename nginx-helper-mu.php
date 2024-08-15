8f813325df6d606a0543245d3e2470114a5492fd
RamJett <ramjett@ramnic.com>
2024-08-15 15:43:08 -0700

    copy nginx-helper.php and update to be mu-plugin without the activate

--- nginx-helper-mu.php
+++ nginx-helper-mu.php
@@ -22,10 +22,63 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
+ * Base URL of plugin
+ */
+if ( ! defined( 'NGINX_HELPER_BASEURL' ) ) {
+	define( 'NGINX_HELPER_BASEURL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Base Name of plugin
+ */
+if ( ! defined( 'NGINX_HELPER_BASENAME' ) ) {
+	define( 'NGINX_HELPER_BASENAME', plugin_basename( __FILE__ ) );
+}
+
+/**
  * Base PATH of plugin
  */
 if ( ! defined( 'NGINX_HELPER_BASEPATH' ) ) {
-	define( 'NGINX_HELPER_BASEPATH', plugin_dir_path( __FILE__ ) . 'nginx-helper/');
+	define( 'NGINX_HELPER_BASEPATH', plugin_dir_path( __FILE__ ) . 'nginx-helper/' );
 }
 
-require NGINX_HELPER_BASEPATH . 'nginx-helper.php';
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require NGINX_HELPER_BASEPATH . 'includes/class-nginx-helper.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    2.0.0
+ */
+function run_nginx_helper() {
+
+	global $nginx_helper;
+
+	$nginx_helper = new Nginx_Helper();
+
+	$path = $nginx_helper_admin->functional_asset_path();
+
+	// Since must-use plugin. We never activate so the activate_nginx_helper never gets ran.
+	if ( ! is_dir( $path ) ) {
+		mkdir( $path );
+	}
+
+	$nginx_helper->run();
+
+	// Load WP-CLI command.
+	if ( defined( 'WP_CLI' ) && WP_CLI ) {
+
+		require_once NGINX_HELPER_BASEPATH . 'class-nginx-helper-wp-cli-command.php';
+		\WP_CLI::add_command( 'nginx-helper', 'Nginx_Helper_WP_CLI_Command' );
+
+	}
+
+}
+run_nginx_helper();
